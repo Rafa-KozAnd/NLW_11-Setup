8944abe595d9b34e169e9f135fa1d1a049ccaca3
Rafael KozAnd <rafandreola@hotmail.com>
2023-01-20 20:59:02 -0300

    .

--- Code/Server/src/server.ts
+++ Code/Server/src/server.ts
@@ -1,27 +1,14 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from "./routes"
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-app.get('/hello', async () => {
-
-    const habits = await prisma.habits.findMany({
-        where: {
-            title: {
-                startsWith: 'Beber'
-            }
-        }
-    })
-
-    return habits
-})
+app.register(appRoutes)
 
 app.listen({
-    port: 3333,
+  port: 3333,
 }).then(() => {
-    console.log('HTTP Server Running!')
+  console.log('HTTP Server running!')
 })
